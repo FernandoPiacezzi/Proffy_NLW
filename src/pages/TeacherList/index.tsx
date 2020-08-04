3465ae3ca60c4786a7385d910964270d16b4c6d5
FernandoPiacezzi <fernandopiacezzi@gmail.com>
2020-08-03 21:43:26 -0300

    landing page

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
+import PageHeader from '../../components/PageHeader';
+
+function TeacherList () {
+    return (
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponíveis." />
+        </div>
+    );
+}
+
+export default TeacherList;
